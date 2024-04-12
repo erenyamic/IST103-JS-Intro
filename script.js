6d9b5a16a2e348320b8ae21fede9fdc7fe487e6c
erenyamic <erenyamic@gmail.com>
2024-04-11 18:16:08 -0700

    My Commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Activity 1
 window.onload=function(){alert("Hello World");};
 
+//Activity 2
 let personName = "Sophia Garcia";
 let jobTitle = "Data Scientist";
 let favoriteHobby = "Photography";
@@ -44,4 +46,40 @@ console.log("Middle name is " + middleName);
 console.log("Spouse's name is " + spouseName); 
 
 console.log("Country of residence: " + country);
+
+//Activity 3
+let toDoList = [
+    "Finish writing a new chapter",
+    "Do laundry",
+    "Practice guitar for an hour",
+    "Call a friend",
+    "Plan meals for the week",
+    "Work on side project"
+];
+
+
+let laptop = {
+    brand: "Apple",
+    model: "MacBook Pro",
+    screenSize: "13 inches",
+    color: "Space Gray",
+    batteryLevel: 85,
+    isTouchScreen: false
+};
+
+
+console.log("To-do list for today:");
+for (let i = 0; i < toDoList.length; i++) {
+    console.log(`${i + 1}. ${toDoList[i]}`);
+}
+
+console.log("\nDescription of my laptop:");
+console.log("Brand:", laptop.brand);
+console.log("Model:", laptop.model);
+console.log("Screen size:", laptop.screenSize);
+console.log("Color:", laptop.color);
+console.log("Battery level:", laptop.batteryLevel + "%");
+console.log("Is it a touch screen laptop?", laptop.isTouchScreen);
+
+
 
