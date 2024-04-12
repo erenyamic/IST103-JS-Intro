dd1caf0d338afd2c2688c27324a5c95b4f8c2fc5
erenyamic <erenyamic@gmail.com>
2024-04-11 18:12:22 -0700

    My Commit

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 window.onload=function(){alert("Hello World");};
+
+let personName = "Sophia Garcia";
+let jobTitle = "Data Scientist";
+let favoriteHobby = "Photography";
+let homeCity = "San Francisco";
+
+let age = 32;
+let yearsOfExperience = 7;
+let numberOfSiblings = 2;
+
+let isMarried = true;
+let hasPets = false;
+
+let heightMeters = 1.70; 
+let accountBalance = 52750.50; 
+
+
+let middleName; 
+
+
+let spouseName = "Carlos"; 
+
+
+const country = "United States"; 
+
+
+console.log("Person's name is " + personName);
+console.log("Job title is " + jobTitle);
+console.log("Favorite hobby is " + favoriteHobby);
+console.log("Home city is " + homeCity);
+
+console.log("Age is " + age);
+console.log("Years of experience in their field: " + yearsOfExperience);
+console.log("Number of siblings: " + numberOfSiblings);
+
+console.log("Is married: " + isMarried);
+console.log("Has pets: " + hasPets);
+
+console.log("Height in meters: " + heightMeters);
+console.log("Account balance in USD: " + accountBalance);
+
+console.log("Middle name is " + middleName); 
+console.log("Spouse's name is " + spouseName); 
+
+console.log("Country of residence: " + country);
+
